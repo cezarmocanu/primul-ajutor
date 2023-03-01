8dc9119c1dea6c76359644f7bb6d8a172cbe5d2f
AleBarboni <alexa_wera@yahoo.com>
2023-03-01 20:55:59 +0200

    added issue 5

---
+++ src/Components/ModuleEntry.jsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import locationIcon from 'src/assets/location-arrow.png'
+import calendar from 'src/assets/calendar.png'
+import clock from 'src/assets/clock.png'
+import price from 'src/assets/money-bill.png'
+import arrowRight from 'src/assets/chevron-right.png'
+
+export default function ModuleEntry (props) {
+    return (
+        <div className="module-card flex border-2 justify-between">
+            <div className="module-details flex justify-start">
+                <div className="left-container flex flex-col px-5">
+                    <div className="module-title  p-3">
+                        <h1 className="font-bold text-2xl pr-5">{props.titluModul}</h1>
+                    </div>
+                    <div className="module-location flex p-3 items-center">
+                        <Image
+                            src={locationIcon}
+                            alt="location"
+                        />
+                        <h3 className="font-semibold pl-2 text-xl">{props.locatieModul}</h3>
+                    </div>
+                </div>
+                <div className="center-container flex flex-col px-5">
+                    <div className="module-data flex p-3 items-center">
+                        <Image
+                            src={calendar}
+                        />
+                        <p className="pl-2">Data: {props.data}</p>
+                    </div>
+                    <div className="module-hour p-3">
+                        <p className="flex justify-center pl-2">Ora: {props.ora}</p>
+                    </div>
+                </div>
+                <div className="right-container flex flex-col px-5">
+                    <div className="module-price flex p-3 items-center">
+                        <Image 
+                            src={price}
+                        />
+                        <p className=" pl-2">Pret: {props.pret} RON</p>
+                    </div>
+                    <div className="module-duration flex p-3 items-center">
+                        <Image
+                            src={clock}
+                        />
+                        <p className="pl-2">Durata: {props.durata} ore</p>
+                    </div>
+                </div>
+            </div>
+            <div className="go-to-extra-details flex align-middle p-10">
+                <button>
+                <Image 
+                    src={arrowRight}
+                />
+                </button>
+                
+            </div>
+            
+            
+        </div>
+    )
+}
---
+++ src/Components/SearchTool.jsx
@@ -0,0 +1,77 @@
+
+import Image from "next/image";
+import ModuleEntry from "./ModuleEntry";
+import box from 'src/assets/box.png';
+import calendarWhite from 'src/assets/calendar-white.png';
+
+export default function SearchTool (props) {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = `${yyyy}-${mm}-${dd}`;
+
+    return (
+        <div>
+            <div className="search-module flex justify-between bg-red-500 px-5 py-2">
+                <div className="flex flex-col px-5 modal-content">
+                    <div className="choose-module flex py-2 items-center">
+                        <Image
+                            src={box}
+                            alt="box"
+                        />
+                        <label className="font-bold text-xl ml-2">Modul</label>
+                    </div>
+                        <select name="Modul" id="modul-select" className='border-2 input-text'>
+                            <option value="">Alege un modul</option>
+                            <option>{props.titluModul}</option>
+                        </select>
+                </div>
+                <div className="modul-data flex">
+                    <div>
+                        <div className="label flex py-2 items-center">
+                            <Image 
+                                src={calendarWhite}
+                                alt="calendar"
+                                />
+                            <label className="ml-2">Din data de:</label>
+                        </div>
+                        <input
+                            type="date"
+                            className='border-2 input-text'
+                            value={today}
+                        />
+                    </div>
+                    <div className="ml-2">
+                        <div className="label flex py-2 items-center">
+                            <Image 
+                                src={calendarWhite}
+                                alt="calendar"
+                            />
+                            <label className="ml-2">Pana in data de:</label>
+                        </div>
+                        <input
+                            type="date"
+                            className='border-2 input-text'
+                            value={today}
+                        />
+                    </div>
+                </div>
+            </div>
+           <ModuleEntry 
+                titluModul="Primul Ajutor al Adultului"
+                locatieModul="Bucuresti"
+                data="14/03/2023"
+                ora="14:00"
+                durata="2"
+                pret="150"
+            />
+        </div>
+    )
+}
+
+{/* <option value="adult">Primul Ajutor al Adultului</option>
+                            <option value="copil">Primul Ajutor al copilului</option>
+                            <option value="comune">Urgente Medicale Comune</option>
+                            <option value="animal">Primul Ajutor al Animalului</option>
+                            <option value="outdoor">Urgente Outdoor si Traumatisme</option> */}
