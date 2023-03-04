6bd643820fb073cbd8a2e81d6979075cdf305048
Groza Zita <adi_groza@yahoo.com>
2023-03-04 15:49:58 +0200

    created footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer (){
+    return (
+        <div className="flex justify-center space-between h-20 border-solid border-t-2 border-t-stone-900 p-2">
+        <div className="mx-5 flex items-center cursor-pointer">Despre</div>
+        <div className="mx-5 flex items-center cursor-pointer">Confidentialitate</div>
+        <div className="mx-5 flex items-center cursor-pointer">Termeni si conditii</div>
+        <div className="mx-5 flex items-center cursor-pointer">Feedback</div>
+        <div className="mx-5 flex items-center cursor-pointer">Centru de ajutor</div>
+      </div> 
+    )
+}
+
+export default Footer;
