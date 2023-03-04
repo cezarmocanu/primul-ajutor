27045b650a7efae5ef54505a104163e50c2f0451
Groza Zita <adi_groza@yahoo.com>
2023-03-04 15:42:58 +0200

    created module card

---
+++ src/Components/ModuleCard.jsx
@@ -0,0 +1,28 @@
+import primAjutorOutdoor from'../assets/primAjutorOutdoor.png'
+
+function ModuleCard (titluModul, descriereModul) {
+    return(
+        <div className="flex-col h-2/3 w-1/3 bg-white rounded-md drop-shadow-xl">
+            <div className='flex justify-center px-5 py-8'>
+                <img
+                src={primAjutorOutdoor}
+                alt="primAjutorOutdoor"
+                className='flex object-cover'          
+                />
+            </div>
+        
+            <div className='flex p-3 justify-center text-lg'>{titluModul}</div> 
+
+            <div className='flex justify-center text-justify px-8'>
+            <p>
+                {descriereModul}
+            </p>
+            </div>
+            <div className='flex justify-center h-1/3 p-8 text-[#E22A2A] text-xl cursor-pointer'>
+                <p>Afla mai multe</p>
+            </div>
+        </div>      
+    )
+}
+
+export default ModuleCard;
