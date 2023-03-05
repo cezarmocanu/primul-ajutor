db7d11e6bbbd70f34fa91ee65827cc63d24db444
Groza Zita <adi_groza@yahoo.com>
2023-03-05 11:55:32 +0200

    I have made the requested changes: destructured the props, renamed the props in english and tested the component

--- src/Components/ModuleCard.jsx
+++ src/Components/ModuleCard.jsx
@@ -1,22 +1,22 @@
-import primAjutorOutdoor from'../assets/primAjutorOutdoor.png'
+import primAjutorOutdoor from'/src/assets/primAjutorOutdoor.png'
 
-function ModuleCard (titluModul, descriereModul) {
+function ModuleCard ({moduleTitle, moduleDescription}) {
     return(
         <div className="flex-col h-2/3 w-1/3 bg-white rounded-md drop-shadow-xl">
             <div className='flex justify-center px-5 py-8'>
-                <img
+                <img                
                 src={primAjutorOutdoor}
                 alt="primAjutorOutdoor"
                 className='flex object-cover'          
                 />
             </div>
         
-            <div className='flex p-3 justify-center text-lg'>{titluModul}</div> 
+            <div className='flex p-3 justify-center text-lg'>{moduleTitle}</div> 
 
             <div className='flex justify-center text-justify px-8'>
-            <p>
-                {descriereModul}
-            </p>
+                <p>
+                    {moduleDescription}
+                </p>
             </div>
             <div className='flex justify-center h-1/3 p-8 text-[#E22A2A] text-xl cursor-pointer'>
                 <p>Afla mai multe</p>
